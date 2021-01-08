ca3f2eae903427016c458053ecc124838905e0f6
jovanovic-nemanja <69627607+jovanovic-nemanja@users.noreply.github.com>
2021-01-09 02:38:50 +0900

    d

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ date_default_timezone_set('Asia/Kolkata');
 | a PHP script and you can easily do that on your own.
 |
 */
-$root = "http://".$_SERVER['HTTP_HOST'];
+$root = "https://".$_SERVER['HTTP_HOST'];
 $root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 $config['base_url'] = $root;
 
